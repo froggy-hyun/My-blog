dab231ca2eeeeb1d2f390e548c95800a521a5a81
froggy-hyun <1233day@naver.com>
2022-09-14 15:46:27 +0000

    test commit

---
+++ posts/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'image',
+        '_type',
+        'title',
+        'view_count',
+        'created_at',
+        'updated_at',
+    )
+    search_fields = (
+    	'title',
+    )
+    
